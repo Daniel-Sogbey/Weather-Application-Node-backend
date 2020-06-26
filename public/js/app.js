fcdf0975cbd3b1ca09658a67590c42568d00d0dc
Daniel Sogbey <sogbeydaniel28@gmail.com>
2020-06-26 16:38:43 -0700

    init

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+const weatherForm = document.querySelector("form")
+const search = document.querySelector("input")
+const messageOne = document.querySelector("#message-1")
+const messageTwo = document.querySelector("#message-2")
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const location = search.value
+  console.log(location)
+
+  messageOne.textContent = "loading..."
+
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageTwo.textContent = data.error
+      } else {
+        messageOne.textContent = data.forecastData
+        messageTwo.textContent = data.address
+      }
+    })
+  })
+})
---
+++ src/app.js
@@ -0,0 +1,94 @@
+const path = require("path")
+const express = require("express")
+const hbs = require("hbs")
+const geocode = require("./utils/geocode")
+const forecast = require("./utils/forecast")
+
+const app = express()
+
+//Define paths for express config
+const publicDirectoryPath = path.join(__dirname, "../public")
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
+
+//Setup handlebars engine and views location
+app.set("view engine", "hbs")
+app.set("views", viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Daniel ",
+  })
+})
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Daniel",
+  })
+})
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    message: "Call us for any help",
+    name: "Daniel",
+  })
+})
+
+app.get("/weather", (req, res) => {
+  const address = req.query.address
+
+  if (!address) {
+    return res.send({
+      errorMessage: "Unable to find location. Try another search",
+    })
+  }
+
+  geocode(address, (error, { latitude, longitude } = {}) => {
+    if (error) {
+      return res.send({
+        error,
+      })
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({
+          error,
+        })
+      }
+      res.send({
+        forecastData,
+        address,
+      })
+    })
+  })
+})
+
+app.get("/help/*", (req, res) => {
+  res.render("error", {
+    title: "404",
+    errorMessage: "Help Article Not Found",
+    name: "Daniel",
+  })
+})
+
+app.get("*", (req, res) => {
+  res.render("error", {
+    title: "404",
+    errorMessage: "Page Not Found",
+    name: "Daniel",
+  })
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(" server running successfully on port" + port)
+})
